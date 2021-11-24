0b66eea769f6da68b41b020b2508cdb63077695a
Vincent Qi <vincent.qi@hotmail.com>
2021-11-23 18:42:40 -0800

    feat: highlight text

--- js/backgroundchange.js
+++ js/backgroundchange.js
@@ -2,6 +2,7 @@ const r = Math.ceil(Math.random() * 4)
 
 const bg =  document.getElementById('background').style;
 
-//bg.background = `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8) ), url(assets/${r}.jpg)`;
-bg.backgroundImage = `url(assets/${r}.jpg)`;
+bg.background = `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.7) ), url(assets/${r}.jpg)`;
+//bg.backgroundImage = `url(assets/${r}.jpg)`;
 bg.backgroundSize = 'cover';
+bg.backgroundRepeat = 'no-repeat'
