4fc0417beb336a0bd6a49cbb10af2cdd65277267
Vincent Qi <vincent.qi@hotmail.com>
2021-11-23 17:40:27 -0800

    feat: area darken instead of full background

--- js/backgroundchange.js
+++ js/backgroundchange.js
@@ -2,5 +2,6 @@ const r = Math.ceil(Math.random() * 4)
 
 const bg =  document.getElementById('background').style;
 
-bg.background = `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8) ), url(assets/${r}.jpg)`;
+//bg.background = `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8) ), url(assets/${r}.jpg)`;
+bg.backgroundImage = `url(assets/${r}.jpg)`;
 bg.backgroundSize = 'cover';
